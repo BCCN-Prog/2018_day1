df6328ca20c3af640ca7adebb65d4e29fa5d70ff
jthuemmel <jannik@bccn-berlin.de>
2018-04-10 16:26:32 +0200

    added test_read_write

--- test_auth.py
+++ test_auth.py
@@ -1,4 +1,13 @@
 from auth import *
+import pytest
+
+
+#pwdb_path = tempfile.gettempdir() / PWDB_FLNAME
+pwdb_path = 'pwdb.pkl'
+try:
+    pwdb_file = open(pwdb_path, 'rb+')
+except FileNotFoundError:
+    pwdb_file = open(pwdb_path, 'wb+')
 
 def test_get_salt():
     salt = get_salt()
@@ -15,3 +24,12 @@ def test_authenticate():
     assert authenticate(name,valid_pw,pwdb)
     assert not authenticate(name,invalid_pw,pwdb)
     assert not authenticate(wrong_name,valid_pw,pwdb)
+
+def test_read_write():
+    salt = get_salt()
+    name = 'user'
+    pw = 'password'
+    pwdb = {name:(pwhash(pw,salt),salt)}
+    write_pwdb(pwdb,pwdb_file)
+    pwdb_read = read_pwdb(pwdb_file)
+    assert pwdb == pwdb_read
