4aeec83f93fd0ee9cb53743c5fd3ef9b5e0b93a0
Bence Bagi <bagi.bence94@gmail.com>
2018-04-10 15:29:07 +0200

    Added dummy database
    Contains tuples of (username, password, salt, hash)

--- test_auth.py
+++ test_auth.py
@@ -1,5 +1,11 @@
 import auth
 
+('daniel', 'vargas', '=2od%]1*9Q', 9966)
+('nadine', 'heere', 'j=U]V.n`l\\', 10976)
+('mayar', 'ali', 'PJM0~RD*|&', 6986)
+('marc', 'vischer', 'nL*E~T&az1', 13112)
+('pooja', 'subramaniam', '%9<:%fSN$}', 18453)
+
 def test_get_salt():
     salts = [auth.get_salt() for i in range(100)]
     assert all([len(salt) == 10 for salt in salts]), 'Some are not 10 characters long'
