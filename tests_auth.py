b97765394954d127109b94a8efaa0ce395ab2f45
Marc Aurel Vischer <vischer@campus.tu-berlin.de>
2018-04-10 16:29:21 +0200

    use fixture database

--- tests_auth.py
+++ tests_auth.py
@@ -1,25 +1,32 @@
 from auth import (authenticate, add_user, read_pwdb, write_pwdb, pwhash, get_salt)
 import pathlib
+import pytest
+import tempfile
 
+#create database as fixture
+@pytest.fixture
+def pwdb_path():
+    USERNAME = "admin"
+    PASSWORD = "admin"
+    SALT =  'HwIqH8YdT}'
+    pwdb = {}
+    pwdb[USERNAME] = (pwhash(PASSWORD,SALT), SALT)
+    FILEPATH = tempfile.gettempdir() / pathlib.Path('test_pwdb.pkl')
+    with open(FILEPATH, 'wb+') as pwdbfile:
+        write_pwdb(pwdb, pwdbfile)
+    return(FILEPATH)
 
-TEST_PWDB_FLNAME = pathlib.Path('test_pwdb.pkl')
-TEST_PWDB_FLNAME_COPY = pathlib.Path('test_pwdb_copy.pkl')
-
-def test_database_exists():
-    assert TEST_PWDB_FLNAME.exists()
-
-def test_read_pwdb():
+def test_read_pwdb(pwdb_path):
 
     username = 'admin'
     salt = 'HwIqH8YdT}'
     hashed_password = 11215
     try:
-        pwdb_file = open(TEST_PWDB_FLNAME, 'rb+')
+        pwdb_file = open(pwdb_path, 'rb+')
     except FileNotFoundError:
-        pwdb_file = open(TEST_PWDB_FLNAME, 'wb+')
+        pwdb_file = open(pwdb_path, 'wb+')
 
     pwdb = read_pwdb(pwdb_file)
-
 
     assert username in pwdb
     assert pwdb[username][1] == salt
@@ -36,57 +43,58 @@ def test_pwhash():
 
 
 
-def test_authenticate_user_in_database():
+def test_authenticate_user_in_database(pwdb_path):
     username = 'admin'
     password = 'admin'
 
     try:
-        pwdb_file = open(TEST_PWDB_FLNAME, 'rb+')
+        pwdb_file = open(pwdb_path, 'rb+')
     except FileNotFoundError:
-        pwdb_file = open(TEST_PWDB_FLNAME, 'wb+')
+        pwdb_file = open(pwdb_path, 'wb+')
 
     pwdb = read_pwdb(pwdb_file)
 
     assert authenticate(username, password, pwdb)
 
-def test_authenticate_user_not_in_database():
+def test_authenticate_user_not_in_database(pwdb_path):
     username = 'user'
     password = 'admin'
 
     try:
-        pwdb_file = open(TEST_PWDB_FLNAME, 'rb+')
+        pwdb_file = open(pwdb_path, 'rb+')
     except FileNotFoundError:
-        pwdb_file = open(TEST_PWDB_FLNAME, 'wb+')
+        pwdb_file = open(pwdb_path, 'wb+')
 
     pwdb = read_pwdb(pwdb_file)
 
     assert not authenticate(username, password, pwdb)
 
-def test_authenticate_wrong_password():
+def test_authenticate_wrong_password(pwdb_path):
     username = 'admin'
     password = 'admin1'
 
     try:
-        pwdb_file = open(TEST_PWDB_FLNAME, 'rb+')
+        pwdb_file = open(pwdb_path, 'rb+')
     except FileNotFoundError:
-        pwdb_file = open(TEST_PWDB_FLNAME, 'wb+')
+        pwdb_file = open(pwdb_path, 'wb+')
+
 
     pwdb = read_pwdb(pwdb_file)
 
     assert not authenticate(username, password, pwdb)
 
 
-def test_add_user():
+def test_add_user(pwdb_path):
     username = 'user1'
     password = 'password1'
 
     try:
-        pwdb_file = open(TEST_PWDB_FLNAME, 'rb+')
+        pwdb_file = open(pwdb_path, 'rb+')
     except FileNotFoundError:
-        pwdb_file = open(TEST_PWDB_FLNAME, 'wb+')
+        pwdb_file = open(pwdb_path, 'wb+')
 
     pwdb = read_pwdb(pwdb_file)
-
+    TEST_PWDB_FLNAME_COPY = tempfile.gettempdir() / pathlib.Path('test_pwdb_copy.pkl')
     pwdb_file_copy = open(TEST_PWDB_FLNAME_COPY, 'wb+')
 
     # creating a copy of the original test database
