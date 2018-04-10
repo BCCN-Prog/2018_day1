997813a35cd3ed0785da9b88d7aeb92ae79a5a4d
niko@pcp18 <nikolai.zaki@bccn-berlin.de>
2018-04-10 16:36:31 +0200

    added test for add_user also

--- test_pass.py
+++ test_pass.py
@@ -7,7 +7,7 @@ def get_rand_string():
     salt_chars = random.choices(CHARS, k=10)
     return ''.join(salt_chars)
 
-def test_getsalt():
+def test_getsalt_type():
     assert type(login.get_salt())==type('sillystring')
 
 def test_auth_user_known_pw_correct():
@@ -58,3 +58,36 @@ def test_auth_user_unkown():
         hash = login.pwhash(p,salt)
         pwdb = {u:(hash,salt)}
         assert login.authenticate(wu,p,pwdb) is False
+
+def test_read_pwdb_nofile():
+    filename = '/tmp/nodatabase.pkl'
+    assert login.read_pwdb(filename) == {}
+
+def test_pwhash_repeatable():
+    for i in range(100):
+        p = get_rand_string()
+        s = login.get_salt()
+        h1 = login.pwhash(p,s)
+        h2 = login.pwhash(p,s)
+        assert h1 == h2
+
+def test_add_user_newuser():
+    u = get_rand_string()
+    p = get_rand_string()
+    salt = get_rand_string()
+    pwdb_file = './tmp/pwdb.pkl'
+    pwdb = {}
+    login.add_user(u,p,salt,pwdb,pwdb_file)
+    hash = login.pwhash(p,salt)
+    target_pwdb = {u:(hash,salt)}
+    pwdb = login.read_pwdb(pwdb_file)
+    assert target_pwdb == pwdb
+    for i in range(10):
+        u = get_rand_string()
+        p = get_rand_string()
+        salt = get_rand_string()
+        login.add_user(u,p,salt,pwdb,pwdb_file)
+        hash = login.pwhash(p,salt)
+        target_pwdb[u] = (hash,salt)
+        pwdb = login.read_pwdb(pwdb_file)
+        assert target_pwdb == pwdb
