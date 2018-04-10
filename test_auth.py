c367db87c18ed60188f4608976d21a88e6c265b6
Bence Bagi <bagi.bence94@gmail.com>
2018-04-10 15:39:52 +0200

    Added database dictionary

--- test_auth.py
+++ test_auth.py
@@ -7,6 +7,10 @@ db.append(('mayar', 'ali', 'PJM0~RD*|&', 6986))
 db.append(('marc', 'vischer', 'nL*E~T&az1', 13112))
 db.append(('pooja', 'subramaniam', '%9<:%fSN$}', 18453))
 
+db_dict = dict()
+for user in db:
+    db_dict[user[0]] = (user[3], user[2])
+
 def test_get_salt():
     salts = [auth.get_salt() for i in range(100)]
     assert all([len(salt) == 10 for salt in salts]), 'Some are not 10 characters long'
